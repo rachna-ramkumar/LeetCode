2067954c5a22e6702815a0a0a9becb4da75cd37a
Rachna Ramkumar <73000514+rachna-ramkumar@users.noreply.github.com>
2021-06-20 00:24:01 -0400

    Time: 56 ms (69.62%), Space: 14.6 MB (9.83%) - LeetHub

--- top-k-frequent-words/top-k-frequent-words.py
+++ top-k-frequent-words/top-k-frequent-words.py
@@ -1,11 +1,13 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]: 
-        hashmap = defaultdict(int)
-        for i in words:
-            hashmap[i] += 1
-            
-        result = [[k,v] for k,v in hashmap.items()]
+        wordCounts = defaultdict(int)
+        
+        for word in words:
+            wordCounts[word] += 1
+        
+        result = [[k,v] for k, v in wordCounts.items()]
         result.sort()
-        result.sort(key = lambda item: item[1], reverse = True)
+        result.sort(key = lambda x : x[1], reverse = True)
+        
         return [item[0] for item in result][:k]
         
