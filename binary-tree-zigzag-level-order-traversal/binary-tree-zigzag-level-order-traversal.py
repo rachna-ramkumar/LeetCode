ce092e469fd28bee9bfb64ee7c1fe81678f38421
Rachna Ramkumar <73000514+rachna-ramkumar@users.noreply.github.com>
2021-07-02 13:30:37 -0400

    Time: 36 ms (37.65%), Space: 14.4 MB (90.34%) - LeetHub

--- binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -5,20 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, root, level, result):
-        if root is None:
-            return
-        if level >= len(result):
-            result.append([])
-        result[level].append(root.val)
-        self.dfs(root.left, level + 1, result)
-        self.dfs(root.right, level + 1, result)
     def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
-        result = []
-        self.dfs(root, 0, result)
-        for level in range(1, len(result), 2):
-            result[level].reverse()
-        return result
-                
+        if root is None:
+            return []
+        res = []
+        queue = deque([root])
+        count = 2
+        while len(queue) > 0:
+            n = len(queue)
+            new_level = []
+            
+            for _ in range(n):
+                node = queue.popleft()
+                new_level.append(node.val)
+                for child in [node.left, node.right]:
+                    if child is not None:
+                        queue.append(child)
+            if count % 2 == 0:
+                res.append(new_level)
+            elif count % 2 != 0:
+                res.append(reversed(new_level))
+            count += 1
+        return res
+
+        
         
         
