79b9229415cc8efae54d2c938d08e0ec812c79d5
Rachna Ramkumar <73000514+rachna-ramkumar@users.noreply.github.com>
2021-07-12 03:43:36 -0400

    Time: 84 ms (94.85%), Space: 17.9 MB (40.17%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.py
+++ deepest-leaves-sum/deepest-leaves-sum.py
@@ -1,15 +1,12 @@
 class Solution:
     def deepestLeavesSum(self, root: TreeNode) -> int:
-        next_level = deque([root,])
+        next_level = deque([root])
         
         while next_level:
-            # prepare for the next level
             curr_level = next_level
             next_level = deque()
             
             for node in curr_level:
-                # add child nodes of the current level
-                # in the queue for the next level
                 if node.left:
                     next_level.append(node.left)
                 if node.right:
