89354b189392b1f69bc9cacb9c1c2ca19b27eedd
Rachna Ramkumar <73000514+rachna-ramkumar@users.noreply.github.com>
2021-07-12 19:03:18 -0400

    Time: 136 ms (13.01%), Space: 14.7 MB (50.90%) - LeetHub

---
+++ sort-the-matrix-diagonally/sort-the-matrix-diagonally.py
@@ -0,0 +1,16 @@
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        diagonals = defaultdict(list)
+        for row in range(m):
+            for col in range(n):
+                diagonals[row - col].append(mat[row][col])
+        for diagonal in diagonals.values():
+            heapq.heapify(diagonal)
+        
+        for row in range(m):
+            for col in range(n):
+                value = heapq.heappop(diagonals[row - col])
+                mat[row][col] = value
+        return mat
