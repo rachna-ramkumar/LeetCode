ab269b1eed7f38bbecbafe481f89ff544dbe4127
Rachna Ramkumar <73000514+rachna-ramkumar@users.noreply.github.com>
2021-07-14 01:21:05 -0400

    Time: 80 ms (91.92%), Space: 16.2 MB (54.46%) - LeetHub

--- merge-intervals/merge-intervals.py
+++ merge-intervals/merge-intervals.py
@@ -1,19 +1,12 @@
-'''
-Approach : sort the interval
-
-
-'''
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda x:x[0])
-        for i in range(len(intervals)):
-            merged = []
-            for interval in intervals:
-                if not merged or merged[-1][1] < interval [0]:
-                    merged.append(interval)
-                else:
-                    merged[-1][1] = max(merged[-1][1], interval[1])
-            return merged
-                
+        intervals.sort(key = lambda x : x[0])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
         
         
